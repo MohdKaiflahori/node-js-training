a93f31aaf854cfdda698c8fd1361773877a58985
MohdkaifLahori <kaif.lahori@habilelabs.ip>
2023-03-09 12:59:26 +0530

    Assignment done

--- module/operation.js
+++ module/operation.js
@@ -4,45 +4,37 @@
  * @param {number} b
  * @returns {number} return sum of a and b
  */
-const addition = (a, b) => {
-  return a + b;
-};
+const addition = (a, b) => a + b;
 /**
  *
  * @param {number} a
  * @param {number} b
  * @returns {number} return subtraction of a and b
  */
-const subtraction = (a, b) => {
-  return a - b;
-};
+const subtraction = (a, b) => a - b;
 /**
  *
  * @param {number} a
  * @param {number} b
  * @returns {number} return multiply of a and b
  */
-const multiply = (a, b) => {
-  return a * b;
-};
+const multiply = (a, b) => a * b;
+
 /**
  *
  * @param {number} a
  * @param {number} b
  * @returns {number} return division of a and b
  */
-const division = (a, b) => {
-  return a / b;
-};
+const division = (a, b) =>  a / b;
 /**
  *
  * @param {number} a
  * @param {number} b
  * @returns {number} return modulas of a and b
  */
-const modulus = (a, b) => {
-  return a % b;
-};
+const modulus = (a, b) => a % b;
+
 module.exports = {
     addition,
     subtraction,
